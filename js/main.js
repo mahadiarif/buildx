51a6424ce72e1ccdc3283e7501752a1965a4c38b
Mahadi Hassan Arif <mdarif.ibit@gmail.com>
2021-08-31 01:40:33 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Mixer Plugin Activation 
+$(document).ready(function(){
+    //Mixer Js
+    var mixer = mixitup('.mixer');
+    //Counte Up Js
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+  var typed = new Typed('.type', {
+    strings: ["e Do Big Things ","e Do Big Things"],
+    typeSpeed: 100
+  });
+  var typed = new Typed('.type1', {
+    strings: ["ith Big Ideas","ith Big Ideas"],
+    typeSpeed: 100
+  });
+  });
+  
